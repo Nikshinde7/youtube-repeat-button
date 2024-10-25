d7dd62291ec1473c143ba9f985f01d955a146e57
Nikshinde7 <shindenikhil336@gmail.com>
2024-10-25 12:05:54 +0530

    feat: Added the repeat button to the DOM

--- content.js
+++ content.js
@@ -1,19 +1,45 @@
 (() => {
+    let youtubeLeftControls, youtubePlayer;
+
     chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         console.log('content.js received message', request)
 
-        const { message, type } = request;
+        const { type } = request;
 
         if(type === 'NEW') {
             newVideoLoaded();
         }
     })
 
-    const newVideoLoaded = () => {
-        const repeatBtnExists = document.getElementById('repeat-btn')[0];
+    const newVideoLoaded = async () => {
+        const repeatBtnExists = document.getElementById('repeat-btn');
 
         if(!repeatBtnExists) {
-            // Add the repeat button
+
+            const repeatBtn = document.createElement("img");
+
+            const repeatBtnContainer = document.createElement('button');
+            repeatBtnContainer.id = 'repeat-btn-container';
+            repeatBtnContainer.className = 'ytp-button';
+            repeatBtnContainer.style.marginLeft = '5px';
+            
+            repeatBtn.src = chrome.runtime.getURL('assets/repeat.svg');
+            repeatBtn.id = 'repeat-btn';
+            repeatBtn.title = 'Repeat the current playing video';
+            repeatBtn.style.width = '22px';
+            repeatBtn.style.height = '100%';
+            repeatBtn.style.cursor = 'pointer';
+
+            youtubeLeftControls = document.getElementsByClassName('ytp-right-controls')[0];
+            youtubePlayer = document.getElementsByClassName('video-stream')[0];
+
+            repeatBtnContainer.appendChild(repeatBtn);
+            youtubeLeftControls.appendChild(repeatBtnContainer);
+            repeatBtn.addEventListener('click', repeatVideo);
         }
+    }
+
+    const repeatVideo = () => {
+        // Do something
     }
 })()
